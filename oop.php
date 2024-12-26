e4a37342b291821b61cc7c75dd5736fc94f1ef2e
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-12-26 18:51:54 -0500

    Add: inheritance

--- oop.php
+++ oop.php
@@ -2,7 +2,7 @@
   class User {
     public $name;
     public $email;
-    private $status = 'active';
+    protected $status = 'active';
 
     public function __construct($name, $email) {
       $this->name = $name;
@@ -24,6 +24,24 @@
     }
   }
 
+  class Admin extends User {
+    public $level;
+
+    public function __construct($name, $email, $level) {
+      parent::__construct($name, $email);
+
+      $this->level = $level;
+    }
+
+    public function login() {
+      echo 'Admin ' . $this->name . ' logged in';
+    }
+
+    public function getStatus() {
+      echo $this->status;
+    }
+  }
+
   $user1 = new User('Alejandro', 'ale@mail.com');
 
   $user1->login();
@@ -31,6 +49,10 @@
   $user->setStatus('inactive');
 
   $user->getStatus();
-  
+
   var_dump($user1);
+
+  $admin = new Admin('Tom Smith', 'tom@mail.com', 5);
+
+  var_dump($admin);
 ?>
